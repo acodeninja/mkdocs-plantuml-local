c46c31a0412cfc97fbb40002b3123ceec699c677
Lawrence <lawrence@acode.ninja>
2024-02-28 11:19:32 +0000

    ci: add tests for hashing strings

---
+++ test/unit/test_string_hash.py
@@ -0,0 +1,23 @@
+import pytest
+
+from mkdocs_plantuml_local.hash import hash_string, hash_list
+
+
+@pytest.mark.parametrize('string,output', [
+    ('test', 'n4bQgYhMfWWaL-qgxVrQFaO_TxsrC4Is0V1sFbDwCgg'),
+])
+def test_hashing_a_string(string, output):
+    assert hash_string(string) == output
+
+
+@pytest.mark.parametrize('string,output', [
+    (
+        ['unit', 'test'],
+        [
+            'OFz9vADsMgMWmUYHecFQmbK7o8rQ5ED_-wjhDfCsueE',
+            'n4bQgYhMfWWaL-qgxVrQFaO_TxsrC4Is0V1sFbDwCgg',
+        ],
+    ),
+])
+def test_hashing_a_list(string, output):
+    assert hash_list(string) == output
